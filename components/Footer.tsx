8e44c37c2fd67c36e145408c180d274c6a8d9a08
amasia <amasi.pro@gmail.com>
2024-05-22 13:14:40 +0300

    implement auth, appwrite and sentry

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -15,29 +15,24 @@ const Footer = ({ user, type = 'desktop' }: FooterProps) => {
 
     return (
         <div className='footer'>
-            <div className={type === 'mobile' ?
-                'footer_name-mobile' : 'footer_name'}>
+            <div className={type === 'mobile' ? 'footer_name-mobile' : 'footer_name'}>
                 <p className='text-xl font-bold text-gray-700'>
-                    {user.name[0]}
+                    {user?.name[0]}
                 </p>
             </div>
             <div className={type === 'mobile' ?
                 'footer_email-mobile' : 'footer_email'}>
                 <h1 className='text-14 truncate font-semibold 
                     text-gray-700'>
-                    {user.name}
+                    {user?.name}
                 </h1>
                 <p className='text-14 truncate font-normal 
                     text-gray-600'>
-                    {user.email}
+                    {user?.email}
                 </p>
             </div>
             <div className='footer_image' onClick={handleLogout}>
-                <Image
-                    src="/icons/logout.svg"
-                    fill
-                    alt='jsm'
-                />
+                <Image src="/icons/logout.svg" fill alt='jsm' />
             </div>
         </div>
     )
